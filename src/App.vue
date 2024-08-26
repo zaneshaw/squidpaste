6d6f757ee3797b2d484ebf43e4c401bdf15258d1
Zane Shaw <zane@squidee.dev>
2024-08-26 16:40:10 +1000

    Setup layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
+	<div class="mx-auto flex h-screen w-[550px] flex-col gap-10 py-10 text-center">
+		<div class="flex flex-col gap-5">
+			<div class="relative mx-auto flex w-fit flex-col">
+				<h1>squidpaste</h1>
+				<span class="absolute -bottom-4 -right-5 text-sm">yummy</span>
+			</div>
+			<h2>Anonymous Text Sharing</h2>
+		</div>
+		<div class="flex grow flex-col gap-2">
+			<div class="grow ring-1 ring-white"></div>
+			<div class="flex flex-col gap-2 text-xs">
+				<div>
+					<p>All pastes are anonymous and stored permanently until deleted.</p>
+					<p>Make sure to setup a password for any paste that contains sensitive information.</p>
+				</div>
+				<p>Pastes are limited to a maximum size of 128 KB.</p>
+			</div>
+		</div>
+		<div class="flex justify-center gap-2">
+			<a href="">New Paste</a>
+			<span>/</span>
+			<a href="">Help</a>
+			<span>/</span>
+			<a href="">API</a>
+			<span>/</span>
+			<a href="">Contact</a>
+			<span>/</span>
+			<a href="">Security Disclaimer</a>
+		</div>
 	</div>
-	<HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
